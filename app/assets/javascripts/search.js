a376fe81545f2868c1b9780c0102a98401280fb1
amritdeepdhungana <amritdhungana@lftechnology.com>
2013-06-13 16:45:05 +0545

    add js file for search

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,21 @@
+
+// $(document).ready(function(){
+//   var name = $('#user_name').val();
+//   $('#user_name').keyup(function(){
+//     console.log('jere');
+//     $.ajax({
+//       data: {name: name},
+//       url: "/friend/search",
+//       // dataType: "json",
+//       success: function(response){
+//           alert("success");
+//            alert(name);
+//            $(".abc").html('name');
+//       },
+//       error:function(){
+//           alert("failure");
+//           $(".abc").html('Sorry No data');
+//       }   
+//     }); 
+// });
+// });
